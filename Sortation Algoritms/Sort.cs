933e19068c76160bf28b781ae7d6ea382cf783b3
John <derij256@gmail.com>
2023-12-25 21:52:05 +0330

    Add Bubble sort and create sort class

---
+++ Sortation Algoritms/Sort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Sortation_Algoritms
+{
+    class Sort
+    {
+        private long timer;
+        
+
+        public int[] BubbleSort(int[] array)
+        {
+            Stopwatch myWatch = new Stopwatch();
+            myWatch.Start();
+           
+            int temp = 0;
+            int n = array.Length;
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < n - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+
+            myWatch.Stop();
+
+            timer = myWatch.ElapsedMilliseconds;
+            return array;
+
+        }
+
+        public long GetTimer
+        {
+            get{
+                return timer;
+            }
+        }
+
+    }
+}
