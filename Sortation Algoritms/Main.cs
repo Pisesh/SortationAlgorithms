f7f6ece336ef7db3e946ac56eb2dd7c4c2a444f5
Mohammad Askari <derij256@gmail.com>
2023-12-26 13:34:29 +0330

    Add main

---
+++ Sortation Algoritms/Main.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sortation_Algoritms
+{
+    class Main
+    {
+        private string sortBy;
+        private long algorithmTime;
+        private string algorithmType;
+
+        Spliter Msp = new Spliter();
+        Sort Mso = new Sort();
+
+        public string SetSortBy
+        {
+            set
+            {
+                sortBy = value;
+            }
+        }
+
+        public long GetAlgorithmTime
+        {
+            get
+            {
+                return algorithmTime;
+            }
+        }
+
+        public string SetAlgorithmType
+        {
+            set
+            {
+                algorithmType = value;
+            }
+        }
+
+        public string Sort(string inputString)
+        {
+            int arrayLength = 0;
+            string final = "";
+            int[] array = new int[arrayLength];
+
+            arrayLength = Msp.Counter(inputString);
+
+            array = Msp.Splitor(inputString, arrayLength);
+
+            if (algorithmType == "Bubble Sort")
+            {
+                if (sortBy == "Asc")
+                {
+                    array = Mso.BubbleSortAsc(array);
+                }
+                else
+                {
+                    array = Mso.BubbleSortDes(array);
+                }
+            }
+
+            algorithmTime = Mso.GetTimer;
+            final = Msp.Concat(array);
+
+            return final;
+
+        }
+
+    }
+}
