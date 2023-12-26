8edbac998f9abfe970c8a8c880e56966c1783a6b
Mohammad Askari <derij256@gmail.com>
2023-12-26 19:49:19 +0330

    change des algorithms structure

--- Sortation Algoritms/Sort.cs
+++ Sortation Algoritms/Sort.cs
@@ -10,31 +10,13 @@ namespace Sortation_Algoritms
     class Sort
     {
         private long timer;
-        
+        Spliter Msp = new Spliter();
 
         public int[] BubbleSortDes(int[] array)
         {
-            Stopwatch myWatch = new Stopwatch();
-            myWatch.Start();
-           
-            int temp = 0;
-            int n = array.Length;
+            array = BubbleSortAsc(array);
+            array = Msp.ReversNumberArray(array);
 
-            for (int i = 1; i < n; i++)
-            {
-                for (int j = 0; j < n - 1; j++)
-                {
-                    if (array[j] < array[j + 1])
-                    {
-                        temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
-                    }
-                }
-            }
-            myWatch.Stop();
-
-            timer = myWatch.ElapsedMilliseconds;
             return array;
 
         }
@@ -93,24 +75,8 @@ namespace Sortation_Algoritms
 
         public int[] SelectionSortDes(int[] array)
         {
-            Stopwatch myWatch = new Stopwatch();
-            myWatch.Start();
-
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                for (int j = i + 1; j < array.Length; j++)
-                {
-                    if (array[i] < array[j])
-                    {
-                        int temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                    }
-                }
-            }
-            myWatch.Stop();
-
-            timer = myWatch.ElapsedMilliseconds;
+            array = SelectionSortAsc(array);
+            array = Msp.ReversNumberArray(array);
 
             return array;
         }
@@ -136,20 +102,9 @@ namespace Sortation_Algoritms
 
         public int[] InsertionSortDsc(int[] array)
         {
-            int t, j, i;
-            for (i = 1; i < array.Length; i++)
-            {
-                t = array[i];
-                for (j = i; j < 0; j--)
-                {
-                    if (t < array[j - 1])
-                    {
-                        break;
-                    }
-                    array[j] = array[j - 1];
-                }
-                array[j] = t;
-            }
+            array = InsertionSortAsc(array);
+            array = Msp.ReversNumberArray(array);
+
             return array;
         }
 
